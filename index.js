b7eb64881b278f2d9565fd8832418cf74560dd18
kanakaraju <kanakarajuyadavmanda@gmail.com>
2023-05-04 12:35:44 +0000

    Movies

---
+++ index.js
@@ -0,0 +1,81 @@
+const express = require("express");
+const path = require("path");
+const { open } = require("sqlite");
+const sqlite3 = require("sqlite3");
+const bcrypt = require("bcrypt")
+
+const app = express();
+app.use(express.json());
+const dbPath = path.join(__dirname, "goodreads.db");
+
+let db = null;
+
+const initializeDBAndServer = async () => {
+  try {
+    db = await open({
+      filename: dbPath,
+      driver: sqlite3.Database,
+    });
+    app.listen(3000, () => {
+      console.log("Server Running at http://localhost:3000/");
+    });
+  } catch (e) {
+    console.log(`DB Error: ${e.message}`);
+    process.exit(1);
+  }
+};
+initializeDBAndServer();
+
+// Get Books API
+app.get("/books/", async (request, response) => {
+  const getBooksQuery = `
+  SELECT
+    *
+  FROM
+    book
+  ORDER BY
+    book_id;`;
+  const booksArray = await db.all(getBooksQuery);
+  response.send(booksArray);
+});
+
+// create user api
+
+app.post("/user/", async (request, respond)=>{
+    const{username,name,password,gender,location } request.body;
+        const hashedPassword = bcrypt.hash(password.10);
+
+        const selectUserQuery = `
+  SELECT 
+    * 
+  FROM 
+    user 
+  WHERE 
+    username = '${username}'`;
+    const dbUser = await db.get(selectUserQuery)
+
+    if (dbUser === undefined){
+        const createUserQuery = `
+  INSERT INTO
+    user (username, name, password, gender, location)
+  VALUES
+    (
+      '${username}',
+      '${name}',
+      '${hashedPassword}',
+      '${gender}',
+      '${location}'  
+    );`;
+     await  db.run(createUserQuery)
+     response.send("user Created Successfully")
+        //create user in user table
+        else{
+            response.status(400)
+            response.send("User already exists")
+            // send invalid username as response
+        }
+
+    }
+
+
+});
